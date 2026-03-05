bd8fb47aa1a395bdb8bfa5cbd7a2050e548851b0
kayetavi <avijitkayet97@gmail.com>
2026-03-05 14:04:49 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,12 +1,25 @@
-async function login() {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+document.getElementById("loginForm")
+.addEventListener("submit", async function (e) {
 
-  const { error } = await supabase.auth.signInWithPassword({
-    email,
-    password,
-  }); 
+  e.preventDefault();
 
-  if (error) alert(error.message);
-  else window.location.href = "dashboard.html";
-}
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value.trim();
+
+  if (!email || !password) {
+    alert("Please enter email and password");
+    return;
+  }
+
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email: email,
+    password: password
+  });
+
+  if (error) {
+    alert(error.message);
+    return;
+  }
+
+  window.location.href = "dashboard.html";
+});
