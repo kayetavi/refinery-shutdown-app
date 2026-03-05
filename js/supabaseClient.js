fdfce10998d1561bbecb325b35cb714bad813ff4
kayetavi <avijitkayet97@gmail.com>
2026-03-05 13:36:00 +0530

    Create supabaseClient.js

---
+++ js/supabaseClient.js
@@ -0,0 +1,3 @@
+const supabaseUrl = "lhktmcqjywduohrwsmzb";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxoa3RtY3FqeXdkdW9ocndzbXpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI2ODgzNzQsImV4cCI6MjA4ODI2NDM3NH0.JYT2qavvtwESRpJNTNmBmg9p78_u-lD8sjslnFaAZgQ";
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
