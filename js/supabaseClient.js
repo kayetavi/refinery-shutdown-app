0f5d2d7845541082df4765ae53ede689f2139707
kayetavi <avijitkayet97@gmail.com>
2026-03-05 23:59:23 +0530

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,4 +1,4 @@
-var supabase = window.supabase.createClient(
+var supabase = window.supabase.createClient( 
   "https://lhktmcqjywduohrwsmzb.supabase.co", 
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxoa3RtY3FqeXdkdW9ocndzbXpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI2ODgzNzQsImV4cCI6MjA4ODI2NDM3NH0.JYT2qavvtwESRpJNTNmBmg9p78_u-lD8sjslnFaAZgQ"
 );
